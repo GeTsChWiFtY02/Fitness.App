3ba7e709d2ac5b2728dfb17597d3b7b6175a549e
GeTsChWiFtY02 <141801363+GeTsChWiFtY02@users.noreply.github.com>
2024-01-27 19:47:08 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,55 +1,74 @@
 const express = require('express');
-const cors = require('cors');
-const app = express()
-const apiRouter = express.Router();
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET = 'neverTell' } = process.env;
-const {getUserById} = require("../db")
-app.use(cors())
-app.get('/products/:id', function (req, res,) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-})
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const client = require('../db/client');
+const { JWT_SECRET = 'neverTell'} = process.env;
+
 // GET /api/health
-apiRouter.get('/health', async () => {
-    console.log('server is running on port 80')
+router.get('/health', async (req, res, next) => {
+  try {
+    const uptime = process.uptime();
+    const {rows: [dbConnection]} = await client.query('SELECT NOW();');
+    const currentTime = new Date();
+    const lastRestart = new Intl.DateTimeFormat('en', {timeStyle: 'long', dateStyle: 'long', timeZone: "America/Los_Angeles"}).format(currentTime - (uptime * 1000));
+    res.send({message: 'healthy', uptime, dbConnection, currentTime, lastRestart});
+  } catch (err) {
+    next(err);
+  }
 });
-apiRouter.use(async (req, res, next) => {
-    const prefix = 'Bearer';
-    const auth = req.header('Authorization');
-    if (!auth) { // nothing to see here
-      next();
-    } else if (auth.startsWith(prefix)) {
-      const token = auth.slice(prefix.length);
-      try {
-        const parsedToken = jwt.verify(token, JWT_SECRET);
-        const id = parsedToken && parsedToken.id
-        if (id) {
-          req.user = await getUserById(id);
-          next();
-        }
-      } catch (error) {
-        next(error);
+
+
+// set `req.user` if possible
+router.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+  
+  if (!auth) { // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+    
+    try {
+      const parsedToken = jwt.verify(token, JWT_SECRET);
+      
+      const id = parsedToken && parsedToken.id
+      if (id) {
+        req.user = await getUserById(id);
+        next();
       }
-    } else {
-      next({
-        name: 'AuthorizationHeaderError',
-        message: `Authorization token must start with ${ prefix }`
-      });
+    } catch (error) {
+      next(error);
     }
-  });
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${ prefix }`
+    });
+  }
+});
+
+router.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+  next();
+});
+
 // ROUTER: /api/users
 const usersRouter = require('./users');
-apiRouter.use('/users', usersRouter);
+router.use('/users', usersRouter);
+
 // ROUTER: /api/activities
 const activitiesRouter = require('./activities');
-apiRouter.use('/activities', activitiesRouter);
+router.use('/activities', activitiesRouter);
+
 // ROUTER: /api/routines
 const routinesRouter = require('./routines');
-apiRouter.use('/routines', routinesRouter);
+router.use('/routines', routinesRouter);
+
 // ROUTER: /api/routine_activities
 const routineActivitiesRouter = require('./routineActivities');
-apiRouter.use('/routine_activities', routineActivitiesRouter);
-module.exports = apiRouter;
+router.use('/routine_activities', routineActivitiesRouter);
+
+module.exports = router;

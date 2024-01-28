699b231a2f26da86044aa7d20ac639475cb3224e
GeTsChWiFtY02 <141801363+GeTsChWiFtY02@users.noreply.github.com>
2024-01-27 19:49:03 -0800

    Update utils.js

--- api/utils.js
+++ api/utils.js
@@ -1,51 +1,127 @@
-function requireUser(req, res, next) {
-    if (!req.user) {
-      res.status(401);
-      next({
-        name: "MissingUserError",
-        message: "You must be logged in to perform this action"
-      });
-    }
-  
-    next();
+const client = require('./client');
+const util = require('./util');
+
+// database functions
+async function getAllActivities(){
+  try {
+    const {rows} = await client.query(`
+      SELECT * FROM activities;
+    `);
+    return rows;
+  } catch (error) {
+    throw error;
   }
-  
-  // takes required parameters as an array, returns a middleware function that sends back a message if they're not present
-  const requiredNotSent = ({ requiredParams, atLeastOne = false }) => {
-    return (req, res, next) => {
-      // for operations that need at least one param. Not all required.
-      if(atLeastOne) {
-        let numParamsFound = 0;
-        for(let param of requiredParams) {
-          if(req.body[param] !== undefined) {
-            numParamsFound++;
-          }
-        }
-        if(!numParamsFound) {
-          next({
-            name: 'MissingParams',
-            message: `Must provide at least one of these in body: ${requiredParams.join(', ')}`
-          })
-        } else {
-          next();
-        }
-      } else {
-        // figure out which ones are not defined, and return them
-        const notSent = [];
-        for(let param of requiredParams) {
-          if(req.body[param] === undefined) {
-            notSent.push(param);
-          }
-        }
-        if(notSent.length) next({
-          name: 'MissingParams',
-          message: `Required Parameters not sent in body: ${notSent.join(', ')}`
-        })
-        next();
-      }
-    }
+}
+async function getActivityById(id){
+  try {
+    const {rows: [activity]} = await client.query(`
+      SELECT * FROM activities
+      WHERE id = $1
+    `, [id]);
+    return activity;
+  } catch (error) {
+    throw error;
   }
+}
+async function getActivityByName(name){
+  try {
+    const {rows: [activity]} = await client.query(`
+      SELECT * FROM activities
+      WHERE name = $1
+    `, [name]);
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
+async function getActivitiesByRoutineId(id) {
+  try {
+    const { rows: activities } = await client.query(`
+    SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId"
+    FROM activities 
+    JOIN routine_activities ON routine_activities."activityId" = activities.id
+    WHERE routine_activities."routineId" = $1;
+  `, [id]);
+  return activities;
+  } catch (error) {
+    throw error;
+  }
+}
+async function attachActivitiesToRoutines(routines) {
+  // no side effects
+  const routinesToReturn = [...routines];
+  const binds = routines.map((_, index) => `$${index + 1}`).join(', ');
+  const routineIds = routines.map(routine => routine.id);
+  if (!routineIds?.length) return [];
   
+  try {
+    // get the activities, JOIN with routine_activities (so we can get a routineId), and only those that have those routine ids on the routine_activities join
+    const { rows: activities } = await client.query(`
+      SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
+      FROM activities 
+      JOIN routine_activities ON routine_activities."activityId" = activities.id
+      WHERE routine_activities."routineId" IN (${ binds });
+    `, routineIds);
+
+    // loop over the routines
+    for(const routine of routinesToReturn) {
+      // filter the activities to only include those that have this routineId
+      const activitiesToAdd = activities.filter(activity => activity.routineId === routine.id);
+      // attach the activities to each single routine
+      routine.activities = activitiesToAdd;
+    }
+    return routinesToReturn;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// select and return an array of all activities
+async function createActivity({ name, description }){
+  try {
+    const {rows: [activity]} = await client.query(`
+      INSERT INTO activities(name, description) VALUES ($1, $2)
+      ON CONFLICT (name) DO NOTHING 
+      RETURNING *
+    `, [name, description]);
+    return activity;
+  } catch (error) {
+    throw error;
+  }}
+// return the new activity
+async function updateActivity({id, ...fields}){
+  try {
+    const toUpdate = {}
+    for(let column in fields) {
+      if(fields[column] !== undefined) toUpdate[column] = fields[column];
+    }
+    let activity;
+    if (util.dbFields(toUpdate).insert.length > 0) {
+      const {rows} = await client.query(`
+        UPDATE activities
+        SET ${ util.dbFields(toUpdate).insert }
+        WHERE id=${ id }
+        RETURNING *;
+      `, Object.values(toUpdate));
+      activity = rows[0];
+    }
+    return activity;
+  } catch (error) {
+    throw error
+  }
+}
+// don't try to update the id
+// do update the name and description
+// return the updated activity
+module.exports = {
+  getAllActivities,
+  getActivityById,
+  getActivityByName,
+  getActivitiesByRoutineId,
+  attachActivitiesToRoutines,
+  createActivity,
+  updateActivity,
+}
   module.exports = {
     requireUser,
     requiredNotSent,

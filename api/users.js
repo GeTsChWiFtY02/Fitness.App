4ce831210abd06c675247b53fcd84e9597713e03
GeTsChWiFtY02 <141801363+GeTsChWiFtY02@users.noreply.github.com>
2024-01-27 19:48:26 -0800

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,132 +1,103 @@
 const express = require('express');
 const router = express.Router();
-// took from juicebox
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const usersRouter = require('../../FitnessTracker/api/users');
-const { getAllUsers, createUser, getUserByUsername } = require('../db.users')
-const {getPublicRoutinesByUser} = require('../db.routines')
-
-
+const { createUser, getUserByUsername, getPublicRoutinesByUser, getAllRoutinesByUser, getUser } = require('../db');
+const { requireUser } = require('./utils');
+const { JWT_SECRET = 'neverTell' } = process.env;
 
 // POST /api/users/login
-//from juicebox; no hashed password
-usersRouter.post('/login', async (req, res, next) => {
+router.post('/login', async (req, res, next) => {
   const { username, password } = req.body;
 
+  // request must have both
   if (!username || !password) {
     next({
-      name: "MissingCredentialsError",
-      message: "Please suppy both a username and password"
+      name: 'MissingCredentialsError',
+      message: 'Please supply both a username and password'
     });
   }
-  //juicebox put in id: 1 and username: 'albert' not sure what we need for this
-  const token = jwt.sign({ id: 1, username: 'albert' }, process.env.JWT_SECRET);
 
   try {
-    const user = await getUserByUsername(username, password);
-    const SALT_COUNT = 10;
-    const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
-
-    if (!user && !user.password == hashedPassword) {
-      res.send({
-        name: 'IncorrectCredentialsError',
-        message: 'Username or password is incorrect'
-      });
-
-    } else {
+    const user = await getUser({username, password});
+    if(!user) {
       next({
-        message: "you're logged in!", token
-
-      });
+        name: 'IncorrectCredentialsError',
+        message: 'Username or password is incorrect',
+      })
+    } else {
+      const token = jwt.sign({id: user.id, username: user.username}, JWT_SECRET, { expiresIn: '1w' });
+      res.send({ user, message: "you're logged in!", token });
     }
   } catch (error) {
-    console.log(error);
     next(error);
   }
 });
 
 // POST /api/users/register
-// from juicebox; did not hash password, took out name and location; added password.length if statement
-usersRouter.post('/register', async (req, res, next) => {
-  const { username, password } = req.body;
-
+router.post('/register', async (req, res, next) => {
   try {
-    const _user = await getUserByUsername(username);
-    const SALT_COUNT = 10;
-    const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
-
-    if (_user) {
+    const {username, password} = req.body;
+    const queriedUser = await getUserByUsername(username);
+    if (queriedUser) {
+      res.status(401);
       next({
         name: 'UserExistsError',
         message: 'A user by that username already exists'
       });
-    }
-
-    const user = await createUser({username, hashedPassword});
-
-    if (password.length < 8) {
+    } else if (password.length < 8) {
+      res.status(401);
       next({
-        password: "Password must be at least 8 characters"
+        name: 'PasswordLengthError',
+        message: 'Password Too Short!'
       });
+    } else {
+      const user = await createUser({
+        username,
+        password
+      });
+      if (!user) {
+        next({
+          name: 'UserCreationError',
+          message: 'There was a problem registering you. Please try again.',
+        });
+      } else {
+        const token = jwt.sign({id: user.id, username: user.username}, JWT_SECRET, { expiresIn: '1w' });
+        res.send({ user, message: "you're signed up!", token });
+      }
     }
-
-    const token = jwt.sign({
-      id: user.id,
-      username
-    }, process.env.JWT_SECRET, {
-      expiresIn: '1w'
-    });
-
-    res.send({
-      message: "thank you for signing up",
-      token
-    });
-  } catch ({ name, message }) {
-    next({ name, message })
+  } catch (error) {
+    next(error)
   }
-});
-
-
-
-
+})
 
 // GET /api/users/me
-// from juicebox usersRouter.get to start. Created const userData... myself
-usersRouter.get('/users', async (req, res, next) => {
-  const { username } = req.params;
+router.get('/me', requireUser, async (req, res, next) => {
   try {
-    const token = jwt.sign({username}, process.env.JWT_SECRET, {expiresIn:'1w'});
-    const currentUser = await getAllUsers({username,token});
-      if(!token) {
-        next({
-          message: "Invalid credentials"
-        })
-        res.send({currentUser})
-      } 
-    } catch (error) {
-        next(error);
-      }
-});
-  
+    res.send(req.user);
+  } catch (error) {
+    next(error)
+  }
+})
 
 // GET /api/users/:username/routines
-// used usersRouter.get from juicebox users.js as a start
-usersRouter.get('/:username/routines', async (req, res, next) => {
-  const {username} = req.params;
-  try{
-    const userRoutines = await getPublicRoutinesByUser(username);
-    if(!username) {
+router.get('/:username/routines', async (req, res, next) => {
+  try {
+    const {username} = req.params;
+    const user = await getUserByUsername(username);
+    if(!user) {
       next({
-        username: "username does not exist",
-        routines: "public routines do not exist",
-        message: "There are no public routines for this user"
+        name: 'NoUser',
+        message: `Error looking up user ${username}`
       });
-      res.send(userRoutines)
+    } else if(req.user && user.id === req.user.id) {
+      const routines = await getAllRoutinesByUser({username: username});
+      res.send(routines);
+    } else {
+      const routines = await getPublicRoutinesByUser({username: username});
+      res.send(routines);
     }
-  } catch ({message}) {
-    return (username)
+  } catch (error) {
+    next(error)
   }
-});
-
+})
 module.exports = router;
